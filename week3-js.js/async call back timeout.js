21df30548cd597d2cf7de2693e2dcafd292f2515
Mohfazam <mohd.sarwar.code@gmail.com>
2024-09-23 01:25:00 +0530

    timeout function vs IO operation priority

---
+++ week3-js.js/async call back timeout.js
@@ -0,0 +1,13 @@
+function print(){
+    console.log("Inside the print function");
+}
+
+console.log("Hi");
+
+setTimeout(print, 2000);
+
+for(let i = 0; i< 5000000000; i++){
+    let c = i;
+}
+
+console.log("operation done");
